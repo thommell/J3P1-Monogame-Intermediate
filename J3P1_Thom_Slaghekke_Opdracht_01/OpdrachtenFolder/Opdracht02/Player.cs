ab40264fc53faa86e163b52dc7e0819bfc59caa3
thommell <thomslaghekke@outlook.com>
2023-12-08 16:49:46 +0100

    end of day 8/12/2023
    player will now always go back to his original position after u hit the gate

--- J3P1_Thom_Slaghekke_Opdracht_01/OpdrachtenFolder/Opdracht02/Player.cs
+++ J3P1_Thom_Slaghekke_Opdracht_01/OpdrachtenFolder/Opdracht02/Player.cs
@@ -19,11 +19,13 @@ public class Player : GameObject
     private readonly Viewport _viewPort;
     protected PlayerItems _items = PlayerItems.Normal;
     private Texture2D[] _playerTextures;
+    private Vector2 _originalPosition;
     public Player(Vector2 pPosition, Texture2D pTexture, Rectangle pRectangle, Viewport pViewPort, Texture2D[] pPlayerTextures, Game1 pGame) : base(pPosition, pTexture, pRectangle)
     {
         _viewPort = pViewPort;
         _playerTextures = pPlayerTextures;
         _game1 = pGame;
+        _originalPosition = pPosition;
     }
     public override void UpdateObject(GameTime pGameTime)
     {
@@ -104,7 +106,9 @@ public class Player : GameObject
         }
         else if (pObjectName == "gate")
         {
-            _game1.Exit();
+            //_game1.Exit();
+            _game1.SwapScene("menu");
+            SetPlayerToOriginalPosition();
         }
         else
         {
@@ -112,6 +116,10 @@ public class Player : GameObject
         }
 
         UpdateTexture();
+    }
+    private void SetPlayerToOriginalPosition()
+    {
+        _position = new Vector2(_originalPosition.X, _originalPosition.Y);
     }
 
 }
